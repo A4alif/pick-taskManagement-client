b1c11de49c9a489ce41409820795146cdd10ac9c
alifhasanshah <alifhasanshah@gmail.com>
2023-12-21 01:18:17 +0600

    create react app and install react router dom tailwind daisyui react hook form tanstack query axios

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-5xl font-bold text-red-600">Welcome to Task Management System</h2>
+    </>
+  );
+}
+
+export default App;
